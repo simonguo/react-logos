bfcea4034292ee49fe43653ad528f3681edf69f9
simonguo <simonguo.2009@gmail.com>
2016-09-18 00:04:27 +0800

    Added Google and Apple

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import { Hypers, GuruDigital } from '../src';
+import { Hypers, GuruDigital, Google, Apple} from '../src';
+import './style.less';
+
+class Item extends Component {
+    render() {
+        const { title, children } = this.props;
+        return (
+            <li>
+                {children}
+                <span>{title}</span>
+            </li>
+        );
+    }
+}
+
 
 render((
-    <div>
-        <p>Hypers</p>
-        <Hypers  width={90} color="#00c3ee" backgroundColor="transparent"/>
-        <p>Guru Digital</p>
-        <GuruDigital  width={90} color="#fff" backgroundColor="#00b12b"  />
-    </div>
+    <ul className="container">
+        <Item title="Hypers">
+            <Hypers  width={90} color="#00c3ee" backgroundColor="transparent"/>
+        </Item>
+
+        <Item title="Guru Digital">
+            <GuruDigital  width={90} color="#fff" backgroundColor="#00b12b"  />
+        </Item>
+
+        <Item title="Google">
+            <Google  width={180}  height={60}/>
+        </Item>
+
+        <Item title="Apple">
+            <Apple  width={90}  height={90} color="#999" />
+        </Item>
+
+
+    </ul>
 ), document.getElementById('mount'));
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export Hypers from './Hypers';
 export GuruDigital from './GuruDigital';
+export Google from './Google';
+export Apple from './Apple'
